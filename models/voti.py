32a0e8ef31f6e505c2c436c76e92815842f408a7
salus-utis <57659134+salusutis@users.noreply.github.com>
2020-02-25 22:48:41 +0100

    Update voti.py

--- models/voti.py
+++ models/voti.py
@@ -1,1 +1,15 @@
+from odoo import models, fields
 
+class scuola_voti(models.Model):
+    _name= 'scuola.voti'
+    _description= 'Voti Record'
+
+    voto_alunno = fields.Float(digits=(2,2), string='Voto', required= True)
+    voto_materia = fields.Char(string='Materia', required= True)
+    voto_data = fields.Date(string='Data voto', required= True)
+    
+    alunno_id = fields.Many2one(
+        'scuola.alunno', 
+        string='Alunni'
+    )
+    
