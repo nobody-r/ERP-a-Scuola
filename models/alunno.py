5f480cb15edcc518bf26c39cfc1b3c5a6730d472
nobody-r <coparei7@gmail.com>
2020-02-27 19:40:57 +0100

    votes wip

--- models/alunno.py
+++ models/alunno.py
@@ -9,15 +9,3 @@ class scuola_alunno(models.Model):
     anni_alunno= fields.Char(string='Anni', required= True)
     note_alunno= fields.Text(string='Note')
     foto_alunno= fields.Binary(string='Foto alunno')
-"""
-    @api.multi
-    def open_student_votes(self):
-        return {
-            'name': _('Voti'),
-            'domain': [('alunno_id', '=', self.id)],
-            'view_type': 'form',
-            'res_model': 'scuola.voti',
-            'view_id': False,
-            'view_mode': 'tree,form',
-            'type': 'ir.actions.act_window',
-        }
