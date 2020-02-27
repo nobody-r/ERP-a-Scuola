50219c2609e54965d617fa76ea3d898562169fb4
nobody-r <coparei7@gmail.com>
2020-02-27 22:42:16 +0100

    Smart button

--- models/alunno.py
+++ models/alunno.py
@@ -10,14 +10,14 @@ class scuola_alunno(models.Model):
     note_alunno= fields.Text(string='Note')
     foto_alunno= fields.Binary(string='Foto alunno')
 
-@api.multi
+    @api.multi
     def open_student_votes(self):
-        return {
-            'name': _('Voti'),
-            'domain': [('alunno_id', '=', self.id)],
-            'view_type': 'form',
-            'res_model': 'scuola.voti',
-            'view_id': False,
-            'view_mode': 'tree,form',
-            'type': 'ir.actions.act_window',
-        }
+    return {
+    'name': _('Voti'),
+    'domain': [('alunno_id', '=', self.id)],
+    'view_type': 'form',
+    'res_model': 'scuola.voti',
+    'view_id': False,
+    'view_mode': 'tree,form',
+    'type': 'ir.actions.act_window',
+    }
