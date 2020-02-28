18f5856e461d060c04d12d222b59bb182b041a1c
nobody-r <coparei7@gmail.com>
2020-02-28 14:35:16 +0100

    Smart button

--- models/alunno.py
+++ models/alunno.py
@@ -3,7 +3,6 @@ from odoo import models, fields, api
 class scuola_alunno(models.Model):
     _name= 'scuola.alunno'
     _description= 'Alunno Record'
-
     nome_alunno= fields.Char(string='Nome', required= True)
     cognome_alunno= fields.Char(string='Cognome', required= True)
     anni_alunno= fields.Char(string='Anni', required= True)
