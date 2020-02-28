70d979d6732a7d001f5317a81e0b9a3e245057aa
nobody-r <coparei7@gmail.com>
2020-02-28 16:22:11 +0100

    modulo_scuola

--- models/classe.py
+++ models/classe.py
@@ -4,4 +4,4 @@ class alunno_classe(models.Model):
     _name = 'alunno.classe'
     _description = 'Classe Record'
 
-    alunno_id = fields.many2one('scuola.alunno', string='Alunni')
+    alunno_id = fields.One2many('scuola.alunno', string='Alunni')
