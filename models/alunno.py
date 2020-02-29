3735eb14d1a85b6d18e8f5c9fb4387f63784f6e3
nobody-r <coparei7@gmail.com>
2020-02-29 14:35:17 +0100

    test

--- models/alunno.py
+++ models/alunno.py
@@ -3,13 +3,20 @@ from odoo import models, fields, api, _
 class scuola_alunno(models.Model):
     _name= 'scuola.alunno'
     _description= 'Alunno Record'
-    _rec_name= 'nome_alunno'
+    _rec_name= 'combination'
+
 
     nome_alunno= fields.Char(string='Nome', required= True)
     cognome_alunno= fields.Char(string='Cognome', required= True)
     anni_alunno= fields.Char(string='Anni', required= True)
     note_alunno= fields.Text(string='Note')
     foto_alunno= fields.Binary(string='Foto alunno')
+    combination = fields.Char(string='Combination', compute='fields_combination')
+
+    @api.depends('nome_alunno', 'cognome_alunno')
+    def fields_combination(self):
+        for test in self:
+            test.combination = nome_alunno + ' ' + cognome_alunno
 
     @api.multi
     def alunno_voti(self):
