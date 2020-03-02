26f2ba36b709b0d1b6acee22a9143d83e8d600ad
nobody-r <coparei7@gmail.com>
2020-03-02 15:00:19 +0100

    modulo_scuola

--- models/classi.py
+++ models/classi.py
@@ -3,6 +3,7 @@ from odoo import models, fields, api, _
 class scuola_classi(models.Model):
     _name= 'scuola.classi'
     _description= 'Classi Record'
+    _rec_name = 'nome_classe'
 
     classe_id= fields.One2many('scuola.alunno','classe_alunno', string='Classe')
     nome_classe= fields.Char(string='Nome Classe', required= True)
