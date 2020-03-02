43b9d4482d8bb09be71f8e507def9493c2c5db80
nobody-r <coparei7@gmail.com>
2020-03-02 11:06:56 +0100

    modulo_scuola

--- models/voti.py
+++ models/voti.py
@@ -5,7 +5,16 @@ class scuola_voti(models.Model):
     _description= 'Voti Record'
 
     voto_alunno = fields.Float(digits=(2,2), string='Voto', required= True)
-    voto_materia = fields.Char(string='Materia', required= True)
+    voto_materia = fields.Selection([
+        ('informatica', 'Informatica'),
+        ('ec.aziendale', 'Ec.Aziendale'),
+        ('storia', 'Storia'),
+        ('italiano', 'Italiano'),
+        ('dirito', 'Diritto'),
+        ('ec.politica', 'Ec.Politica'),
+        ('religione', 'Religione'),
+        ('ed.fisica', 'Ed.Fisica'),
+    ], required= True)
     voto_data = fields.Date(string='Data voto', required= True)
     alunno_id = fields.Many2one('scuola.alunno', string="Alunni")
 
