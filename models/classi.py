783c014aa9cdede3307969f962e7beaa6a416527
nobody-r <coparei7@gmail.com>
2020-03-02 15:18:36 +0100

    modulo_scuola

--- models/classi.py
+++ models/classi.py
@@ -5,5 +5,5 @@ class scuola_classi(models.Model):
     _description= 'Classi Record'
     _rec_name = 'nome_classe'
 
-    classe_id= fields.One2many('scuola.alunno','classe_alunno', string='Classe')
-    nome_classe= fields.Char(string='Nome Classe', required= True)
+    classe_id= fields.One2many('scuola.alunno','classe_alunno')
+    nome_classe= fields.Char(string='Classe', required= True)
