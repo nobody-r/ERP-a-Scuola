5f6f47f33b3e81b1435e0626849daca0c88a0b1d
nobody-r <coparei7@gmail.com>
2020-03-02 10:57:22 +0100

    assenze_lines

--- models/alunno.py
+++ models/alunno.py
@@ -48,7 +48,7 @@ class scuola_assenze(models.Model):
     _description= 'Assenze Record'
 
     as_alunno_id = fields.Many2one('scuola.alunno', string="Assenza")
-    assenza_data = fields.Date(string='Assenza', required=True)
+    assenza_data = fields.Date(string='Data', required=True)
     assenza_giustificata= fields.Selection([
         ('ingiustificata', 'Ingiustificata'),
         ('Giustificata', 'Giustificata'),
