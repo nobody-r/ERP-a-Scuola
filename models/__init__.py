136ff377560cdd50dcf2f6f5727624a15838492e
salus-utis <57659134+salusutis@users.noreply.github.com>
2020-03-07 20:13:28 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,7 @@ from . import alunno
 from . import voti
 from . import assenze
 from . import classi
-#from . import presenze
-#from . import hr_alunno
+from . import hr_presenze
+from . import hr_alunni
 
 
