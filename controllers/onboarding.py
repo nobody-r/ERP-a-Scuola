4beb7201c4765802501c461594c2c55ff51ed89b
salus-utis <57659134+salusutis@users.noreply.github.com>
2020-03-13 12:37:08 +0100

    Update onboarding.py

--- controllers/onboarding.py
+++ controllers/onboarding.py
@@ -2,9 +2,9 @@ from odoo import http
 from odoo.http import request
 
 
-class OnboardingController(http.Controller):
+class RegistroOnboardingController(http.Controller):
 
-    @http.route('/registro/account_invoice_onboarding', auth='user', type='json')
+    @http.route('/ERP-a-Scuola/account_invoice_onboarding', auth='user', type='json')
     def account_invoice_onboarding(self):
         """ Returns the `banner` for the account invoice onboarding panel.
             It can be empty if the user has closed it or if he doesn't have
@@ -22,7 +22,7 @@ class OnboardingController(http.Controller):
             })
         }
 
-    @http.route('/registro/account_dashboard_onboarding', auth='user', type='json')
+    @http.route('/ERP-a-Scuola/account_dashboard_onboarding', auth='user', type='json')
     def account_dashboard_onboarding(self):
         """ Returns the `banner` for the account dashboard onboarding panel.
             It can be empty if the user has closed it or if he doesn't have
