db614f086a4aa62986ea3cf929332a09a81b8fbf
salus-utis <57659134+salusutis@users.noreply.github.com>
2020-03-14 14:49:37 +0100

    Update onRegistro.py

--- controllers/onRegistro.py
+++ controllers/onRegistro.py
@@ -1,15 +1,11 @@
 from odoo import http
 from odoo.http import request
-
+import loggingggggg
 
 class RegistroOnboardingController(http.Controller):
     @http.route('/ERP-a-Scuola/scuola_registro_onboarding', auth='user', type='json')
     @serialize_exception
     def scuola_registro_onboarding(self):
-        """ Returns the `banner` for the account invoice onboarding panel.
-            It can be empty if the user has closed it or if he doesn't have
-            the permission to see it. """
-
         company = request.env.company
         if not request.env.is_admin() or \
                 company.account_invoice_onboarding_state == 'closed':
@@ -24,13 +20,9 @@ class RegistroOnboardingController(http.Controller):
 
     @http.route('ERP-a-Scuola/registro_dashboard_onboarding', auth='public', type='json')
     def registro_dashboard_onboarding(self):
-        """ Returns the `banner` for the account dashboard onboarding panel.
-            It can be empty if the user has closed it or if he doesn't have
-            the permission to see it. """
         company = request.env.company
 
-        if not request.env.is_admin() or \
-                company.account_dashboard_onboarding_state == 'closed':
+        if not request.env.is_admin() or company.account_dashboard_onboarding_state == 'closed':
             return {}
 
         return {
